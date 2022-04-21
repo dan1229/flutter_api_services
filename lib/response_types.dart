beb1ff77fe3c929597cf5c3bf35f3cdd93b2c6c9
Daniel Nazarian <danielnazarian@outlook.com>
2022-04-21 17:09:37 -0400

    'finished' types

--- lib/response_types.dart
+++ lib/response_types.dart
@@ -1,13 +1,18 @@
-
-
 class ApiResponse {
+  String message;
+  bool error;
 
+  ApiResponse({this.message = 'Default API response.', this.error = false});
 }
 
 class ApiResponseSuccess extends ApiResponse {
+  dynamic results;
 
+  ApiResponseSuccess({String message = "Successful request.", bool error = false, this.results}) : super(message: message, error: error);
 }
 
-class ApiResponseSError extends ApiResponse {
+class ApiResponseError extends ApiResponse {
+  dynamic results;
 
+  ApiResponseError({String message="Error. Please try again later.", bool error=true, this.results}) : super(message: message, error: error);
 }
