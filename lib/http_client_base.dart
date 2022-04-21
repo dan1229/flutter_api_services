72a690e70c8fef35b660750b4ab8f2c896446889
Daniel Nazarian <danielnazarian@outlook.com>
2022-04-21 16:20:45 -0400

    made params optional and named

--- lib/http_client_base.dart
+++ lib/http_client_base.dart
@@ -15,23 +15,23 @@ import 'package:http/http.dart';
 /// much easier down the line.
 ///
 class HttpClientBase extends BaseClient {
-  HttpClientBase(this.delegate, this.logRequests);
+  HttpClientBase({required this.client, this.logRequests = true});
 
-  final Client delegate;
-  bool logRequests = true;
+  final Client client;
+  final bool logRequests;
 
   @override
   Future<StreamedResponse> send(BaseRequest request) {
     if (logRequests) {
       _logRequest(request);
     }
-    return delegate.send(request).timeout(const Duration(seconds: 10), onTimeout: () {
+    return client.send(request).timeout(const Duration(seconds: 10), onTimeout: () {
       throw Exception("Timeout error.");
     });
   }
 
   @override
-  void close() => delegate.close();
+  void close() => client.close();
 
   void _logRequest(BaseRequest request) {
     logApiPrint(request.toString(), tag: "REQ");
