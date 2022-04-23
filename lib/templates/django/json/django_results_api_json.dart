7b3455db674573ccb583d34c0e9a85fad122ca59
Daniel Nazarian <danielnazarian@outlook.com>
2022-04-23 18:16:03 -0400

    added type to fromJson

--- lib/templates/django/json/django_results_api_json.dart
+++ lib/templates/django/json/django_results_api_json.dart
@@ -18,8 +18,8 @@ class DjangoResultsApiJson<T> {
   ///
   /// JSON
   ///
-  factory DjangoResultsApiJson.fromJson({required Map<String, dynamic> json, required Function fromJson}) => DjangoResultsApiJson<T>(
+  factory DjangoResultsApiJson.fromJson({required Map<String, dynamic> json, Function? fromJson}) => DjangoResultsApiJson<T>(
     message: json["message"],
-    results: json["results"] == null ? null : fromJson(json['results']),
+    results: json["results"] == null ? null : fromJson == null ? json['results'] : fromJson(json['results']),
   );
 }
