24c10b266fd4463ab6c8108d95e205e1fabe2f18
Daniel Nazarian <danielnazarian@outlook.com>
2022-04-24 02:10:27 -0400

    removed print

--- lib/templates/django/django_results_service.dart
+++ lib/templates/django/django_results_service.dart
@@ -320,7 +320,6 @@ class DjangoResultsService<T> {
     }
 
     // process response
-    print(decoded);
     DjangoResultsApiJson res = DjangoResultsApiJson.fromJson(json: decoded, fromJson: fromJson);
     if (response.statusCode == 200 || response.statusCode == 201) {
       // 200 -> valid
